d33f3d95a4bd7579872a89f10583d810f4b75668
Tyler Ammons <wammons23@gmail.com>
2020-07-14 12:50:55 -0500

    first commit

--- scripts/Travelers.js
+++ scripts/Travelers.js
@@ -1,17 +1,17 @@
 class Traveler {
-    constructor (name) {
+    constructor(name) {
         this.name = name
         this.food = 1
         this.isHealthy = true
     }
 
-    hunt () {
+    hunt() {
         this.food += 2
     }
 
-    eat () {
+    eat() {
         if (this.food > 0) {
-        this.food -= 1 
+            this.food -= 1
         }
         else {
             this.isHealthy = false
@@ -19,4 +19,44 @@ class Traveler {
 
     }
 }
-    
+
+class Doctor extends Traveler {
+    constructor(name, food, isHealthy) {
+        super(name, food, isHealthy)
+    }
+
+    heal(traveler) {
+        traveler.isHealthy = true
+    }
+
+}
+
+class Hunter extends Traveler {
+    constructor(name, food, isHealthy) {
+        super(name, food, isHealthy)
+        this.food = 2
+    }
+
+    hunt() {
+        this.food += 5
+    }
+
+    eat() {
+        if (this.food > 1) {
+            this.food -= 2
+        } else {
+            this.food = 0
+            this.isHealthy = false
+        }
+
+    }
+    giveFood(traveler, numOfFoodUnits) {
+        if (numOfFoodUnits <= this.food) {
+            traveler.food += numOfFoodUnits
+            this.food -= numOfFoodUnits
+        }
+
+
+    }
+
+}
